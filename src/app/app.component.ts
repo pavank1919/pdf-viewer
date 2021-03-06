3d5b318958f8803922274b9d8f4eb7621686258a
Pavan Kumar Taluri <pavankumart@maveric-systems.com>
2021-03-06 10:17:04 +0530

    Intial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  src = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+
+  pagechanging($event) {
+    alert("PageNumber->" + $event.pageNumber);
+  }
+}
