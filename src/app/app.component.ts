20af48ea62158112f17e89738d24b80dd7b7bfbb
Pavan Kumar Taluri <pavankumart@maveric-systems.com>
2021-03-08 13:46:32 +0530

    Configurations added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,20 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  public configureAlertMessage : Boolean = false;
   src = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
 
   pagechanging($event) {
-    alert("PageNumber->" + $event.pageNumber);
+    if(this.configureAlertMessage)
+      alert("PageNumber->" + $event.pageNumber);
+  }
+
+  configureAlert($event) {
+    this.configureAlertMessage = true;
+  }
+
+  cancelConfigureAlert($event) {
+    this.configureAlertMessage = false;
   }
 }
+
